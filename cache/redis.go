fa95338205283ab374a51cc1a798440ac9d8babb
Che Kwas <che.kwas@outlook.com>
2022-05-20 18:09:44 +0800

    feat: add cache

--- cache/redis.go
+++ cache/redis.go
@@ -1,2 +1,42 @@
 // Package cache is a cache instance builder.
 package cache // import "github.com/che-kwas/iam-kit/cache"
+
+import (
+	"github.com/go-redis/redis/v8"
+	"github.com/spf13/viper"
+)
+
+const ConfigCacheKey = "redis"
+
+// CacheBuilder defines options for building a cache instance.
+type CacheBuilder struct {
+	Addrs    []string
+	Password string
+	Database int
+
+	err error
+}
+
+// NewCacheBuilder is used to build a cache instance.
+func NewCacheBuilder() *CacheBuilder {
+	b := &CacheBuilder{}
+	b.err = viper.UnmarshalKey(ConfigCacheKey, b)
+
+	return b
+}
+
+// Build builds a redis cache instance.
+func (b *CacheBuilder) Build() (*redis.UniversalClient, error) {
+	if b.err != nil {
+		return nil, b.err
+	}
+
+	// rdb is *redis.ClusterClient.
+	rdb := redis.NewUniversalClient(&redis.UniversalOptions{
+		Addrs:    b.Addrs,
+		Password: b.Password,
+		DB:       b.Database,
+	})
+
+	return &rdb, nil
+}
