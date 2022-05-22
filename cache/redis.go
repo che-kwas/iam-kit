b533221bd97d48488972a5d3426b6f8ff4a9a2ba
Che Kwas <che.kwas@outlook.com>
2022-05-22 18:52:23 +0800

    refactor: redis

--- cache/redis.go
+++ cache/redis.go
@@ -2,40 +2,18 @@
 package cache // import "github.com/che-kwas/iam-kit/cache"
 
 import (
+	"github.com/che-kwas/iam-kit/config"
 	"github.com/go-redis/redis/v8"
-	"github.com/spf13/viper"
 )
 
-const ConfigCacheKey = "redis"
-
-// CacheBuilder defines options for building a cache instance.
-type CacheBuilder struct {
-	Addrs    []string
-	Password string
-	Database int
-
-	err error
-}
-
-// NewCacheBuilder is used to build a cache instance.
-func NewCacheBuilder() *CacheBuilder {
-	b := &CacheBuilder{}
-	b.err = viper.UnmarshalKey(ConfigCacheKey, b)
-
-	return b
-}
-
-// Build builds a redis cache instance.
-func (b *CacheBuilder) Build() (*redis.UniversalClient, error) {
-	if b.err != nil {
-		return nil, b.err
-	}
+// NewRedisIns builds a redis cache instance.
+func NewRedisIns(opts *config.RedisOptions) (*redis.UniversalClient, error) {
 
 	// rdb is *redis.ClusterClient.
 	rdb := redis.NewUniversalClient(&redis.UniversalOptions{
-		Addrs:    b.Addrs,
-		Password: b.Password,
-		DB:       b.Database,
+		Addrs:    opts.Addrs,
+		Password: opts.Password,
+		DB:       opts.Database,
 	})
 
 	return &rdb, nil
