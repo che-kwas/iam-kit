e49088e90ef8c629753225e7478059b4e11171c9
Che Kwas <che.kwas@outlook.com>
2022-06-08 16:33:40 +0800

    feat: add resp logs

--- httputil/http_resp.go
+++ httputil/http_resp.go
@@ -4,6 +4,7 @@ package httputil // import "github.com/che-kwas/iam-kit/httputil"
 import (
 	"net/http"
 
+	"github.com/che-kwas/iam-kit/logger"
 	"github.com/gin-gonic/gin"
 	"github.com/marmotedu/errors"
 )
@@ -19,7 +20,9 @@ type ErrResponse struct {
 // WriteResponse writes the response data or an error into HTTP reponse.
 func WriteResponse(c *gin.Context, err error, data interface{}) {
 	if err != nil {
+		logger.L().X(c).Errorw("response", "error", err)
 		coder := errors.ParseCoder(err)
+
 		c.JSON(coder.HTTPStatus(), ErrResponse{
 			Code:    coder.Code(),
 			Message: coder.String(),
@@ -28,5 +31,6 @@ func WriteResponse(c *gin.Context, err error, data interface{}) {
 		return
 	}
 
+	logger.L().X(c).Infow("response", "data", data)
 	c.JSON(http.StatusOK, data)
 }
