596c79122c44f440710a1772ae5a3cc6d8e8ed0a
Che Kwas <che.kwas@outlook.com>
2022-06-13 09:15:46 +0800

    feat: new mongodb

--- mongo/mongo.go
+++ mongo/mongo.go
@@ -3,6 +3,8 @@ package mongo // import "github.com/che-kwas/iam-kit/mongo"
 
 import (
 	"context"
+	"errors"
+	"time"
 
 	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -15,30 +17,50 @@ import (
 const (
 	confKey = "mongo"
 
+	defaultURI         = "mongodb://127.0.0.1:27017"
+	defaultTimeout     = time.Duration(5 * time.Second)
 	defaultMaxPoolSize = 100
 )
 
 // MongoOptions defines options for building a mongo client.
 type MongoOptions struct {
 	URI         string
+	Database    string
+	Username    string
+	Password    string
+	Timeout     time.Duration
 	MaxPoolSize uint64 `mapstructure:"max-pool-size"`
 }
 
 // NewMongoIns creates a mongo client.
-func NewMongoIns(ctx context.Context) (*mongo.Client, error) {
+func NewMongoIns() (*mongo.Client, error) {
 	opts, err := getMongoOpts()
 	if err != nil {
 		return nil, err
 	}
 	logger.L().Debugf("new mongo instance with options: %+v", opts)
 
-	mgoOpts := options.Client().ApplyURI(opts.URI).SetMaxPoolSize(opts.MaxPoolSize)
-	cli, err := mongo.Connect(ctx, mgoOpts)
-	if err != nil {
-		return nil, err
+	ctx, cancel := context.WithTimeout(context.Background(), opts.Timeout)
+	defer cancel()
+
+	cred := options.Credential{
+		AuthSource: opts.Database,
+		Username:   opts.Username,
+		Password:   opts.Password,
 	}
 
-	if err := cli.Ping(ctx, readpref.Primary()); err != nil {
+	mgoOpts := options.Client().ApplyURI(opts.URI).SetAuth(cred).
+		SetMaxPoolSize(opts.MaxPoolSize)
+	cli, err := mongo.Connect(ctx, mgoOpts)
+	if err == nil {
+		err = cli.Ping(ctx, readpref.Primary())
+	}
+
+	if err != nil {
+		if err == ctx.Err() {
+			err = errors.New("connect to mongo timeout")
+		}
+
 		return nil, err
 	}
 
@@ -47,6 +69,8 @@ func NewMongoIns(ctx context.Context) (*mongo.Client, error) {
 
 func getMongoOpts() (*MongoOptions, error) {
 	opts := &MongoOptions{
+		URI:         defaultURI,
+		Timeout:     defaultTimeout,
 		MaxPoolSize: defaultMaxPoolSize,
 	}
 
